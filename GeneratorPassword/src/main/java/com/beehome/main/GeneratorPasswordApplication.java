0ce721225fa3aceee4b470dfe11d4a99fa645f1f
Jefferson Gomes <jefferson242709@gmail.com>
2024-12-04 22:37:12 -0300

    Iniciando implementação do back-end

---
+++ GeneratorPassword/src/main/java/com/beehome/main/GeneratorPasswordApplication.java
@@ -0,0 +1,14 @@
+package com.beehome.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratorPasswordApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeneratorPasswordApplication.class, args);
+		System.out.print("oix");
+	}
+
+}
---
+++ GeneratorPassword/src/test/java/com/beehome/main/GeneratorPasswordApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beehome.main;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratorPasswordApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
